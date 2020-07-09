7a37480c20ac2712896982d69a5aa048fa446c75
sarvesh kumar <58571739+sarveshmaurya306@users.noreply.github.com>
2020-07-09 19:49:39 +0530

    version 0.0.0

---
+++ src/ChatApp/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function App(params) {
+	return(<>
+	<div className="bg-primary">
+		hello world
+	</div>
+	</>)
+};
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import './ChatApp/App.css'
+import App from './ChatApp/App.jsx'
+ReactDom.render(
+	<><App/></>
+	,document.getElementById("root")
+	)
