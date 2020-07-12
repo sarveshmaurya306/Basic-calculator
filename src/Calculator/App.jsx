5a6682c4b43a257c9a1b53589665e497a85738de
sarvesh kumar <58571739+sarveshmaurya306@users.noreply.github.com>
2020-07-12 11:46:13 +0530

    dot added v-1.0.1

--- src/Calculator/App.jsx
+++ src/Calculator/App.jsx
@@ -1,93 +1,95 @@
 import React from 'react'
-var tnumber=[];
+var tnumber = [];
 var numarr;
 export default function App(params) {
-		function Button(props){
+	function Button(props) {
 
-			function Displayvalue(event){
-				if(event.target.value==='AC'){
-					tnumber=[];
-					setResult('');
-				}
-				else{
-					tnumber.push(event.target.value)
-				}
-				numarr=tnumber.join('')
+		function Displayvalue(event) {
+			if (event.target.value === 'AC') {
+				tnumber = [];
+				setResult('');
 			}
-			
-			return(<>
-				<button value={props.type} className="attractive_button px-4 my-3 py-2" onClick={Displayvalue}>{props.type}</button>
-			</>)
+			else {
+				tnumber.push(event.target.value)
+			}
+			numarr = tnumber.join('')
 		}
-	const [result,setResult]=React.useState();
-	const [number, setNumber]=React.useState();
 
-	function Calculate(event){
+		return (<>
+			<button value={props.type} className="btn btn-outline-success px-4 my-3 py-2" style={{fontSize: "19px" }} onClick={Displayvalue}>{props.type}</button>
+		</>)
+	}
+	const [result, setResult] = React.useState();
+	const [number, setNumber] = React.useState();
+
+	function Calculate(event) {
 		setNumber(numarr)
 	}
 
-	setInterval(Calculate,100)
+	setInterval(Calculate, 100)
 
-	function Result(event){
-		var index=0;
-		var op='+';
-		for(var i=0;i<numarr.length;i++){
-			if(numarr[i]==='+' || numarr[i]==='-' || numarr[i]==='*' || numarr[i]==='/'){
-				index=i;
-				op=numarr[i];
+	function Result(event) {
+		var index = 0;
+		var op = '+';
+		for (var i = 0; i < numarr.length; i++) {
+			if (numarr[i] === '+' || numarr[i] === '-' || numarr[i] === '*' || numarr[i] === '/') {
+				index = i;
+				op = numarr[i];
 				break;
 			}
 		}
-		// console.log(typeof(word.join('')))
-		// console.log(index)
-		var first=parseInt(numarr.slice(0,index))
-		var second=parseInt(numarr.slice(index+1,))
-		switch(op){
-			case '+':setResult(first+second);break;
-			case '-':setResult(first-second);break;
-			case '*':setResult(first*second);break;
-			case '/':setResult(first/second);break;
-			default: setResult(" ");
+
+		var first = parseFloat(numarr.slice(0, index))
+		var second = parseFloat(numarr.slice(index + 1,))
+		switch (op) {
+			case '+': setResult(first + second); break;
+			case '-': setResult(first - second); break;
+			case '*': setResult(first * second); break;
+			case '/': setResult(first / second); break;
+			default: alert("cannot operate this...");setResult("");break;
 		}
 	}
-	return(<>
-		<div style={{width:"300px",height:400, background:"white",boxShadow:"2px 2px 10px 13px blue",position:'absolute',top:"50%",left:"50%",transform:"translate(-50%,-50%)"}}>
-			<Display value={number} res={result}/>
+	return (<>
+		<div style={{ width:300, height:450, background: "#fff", boxShadow: "2px 2px 20px 13px blue", position: 'absolute', top: "50%", left: "50%", transform: "translate(-50%,-50%)" }}>
+			<Display value={number} res={result} />
 			<div className="d-flex justify-content-around">
-				<Button type="1"/>
-				<Button type="2"/>
-				<Button type="3"/>
-				<Button type="+" />
+				<Button type="1" />
+				<Button type="2" />
+				<Button type="3" />
+				<Button type="+"/>
 			</div>
 			<div className="d-flex justify-content-around">
-				<Button type="4"/>
-				<Button type="5"/>
-				<Button type="6"/>
-				<Button type="-"/>
+				<Button type="4" />
+				<Button type="5" />
+				<Button type="6" />
+				<Button type="-" />
 			</div>
 			<div className="d-flex justify-content-around">
-				<Button type="7"/>
-				<Button type="8"/>
-				<Button type="9"/>
+				<Button type="7" />
+				<Button type="8" />
+				<Button type="9" />
 				<Button type="*" />
 			</div>
 			<div className="d-flex justify-content-around">
-				<Button type="AC"/>
-				<Button type="0"/>
-				<button className="attractive_button px-4 my-3 py-2" onClick={Result}>=</button>
-				<Button type="/"/>
+				<Button type="AC" />
+				<Button type="0" />
+				<Button type="." />
+				<Button type="/" />
 			</div>
+				<center><button className="btn btn-outline-danger d-flex justify-content-center attractive_button px-4 py-2" style={{width:200}} onClick={Result}>=</button></center>
 		</div>
 	</>)
 };
 
-
-function Display(props){
-	return(<>
-		<div style={{height:"20%",backgroundColor:"#86a095",borderBottom:"1px solid black"}}>
-			<span className="mx-2 my-5" style={{fontFamily:"Orbitron",fontSize:"24px"}}>
-				{props.value}<br/>{props.res}
+function Display(props) {
+	return (<>
+		<div style={{ height: "17%", backgroundColor: "#86a095", borderBottom: "2px solid black" }}>
+			<span className="mx-2 my-5" style={{ fontWeight: 700, letterSpacing: 2, fontSize: "24px" }}>
+				{props.value}
+			</span><br />
+			<span className="mx-2 my-5" style={{ fontWeight: 700, letterSpacing: 2, fontSize: "24px" }}>
+				{props.res}
 			</span>
 		</div>
-		</>)
+	</>)
 }
